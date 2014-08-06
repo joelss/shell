9e45fd812bed66401bf3f1ed7460be20aecd5dcd
Jay <smile665@gmail.com>
2014-08-06 20:59:04 +0800

    remove some unused info

--- sh2014/disable_jenkins_jobs.sh
+++ sh2014/disable_jenkins_jobs.sh
@@ -1,17 +1,11 @@
-#!binbash
-jobs="dptest-auto-ba-productcenter-test    dptest-auto-main-groupback           dptest-auto-main-shop
-dptest-auto-booking-api              dptest-auto-main-mylist              dptest-auto-main-user
-dptest-auto-core                     dptest-auto-main-page                dptest-auto-tuan-api
-dptest-auto-main-activityback        dptest-auto-main-piccenter           dptest-auto-tuan-buy
-dptest-auto-main-common              dptest-auto-main-piccenter-upload    dptest-auto-tuan-mygroupon
-dptest-auto-main-contentverify       dptest-auto-main-poi                 dptest-auto-tuan-paycenter-interface
-dptest-auto-main-group               dptest-auto-main-promo               dptest-auto-tuan-verify"
+#!/bin/bash
+jobs="my-test-job-1 my-test-job-2 my-test-job-3"
 
 user="user"
 password="password"
 curl="curl --user $user:$password"
 curl="curl"
-jenkins_url="http://alpha.ci.dp"
+jenkins_url="http://myjenkins.com"
 for j in $jobs
 do
   disable_url="$curl -o /dev/null --data disable $jenkins_url/job/$j/disable"
